714470bd2e6559ffc36642f4601e6f05c555a6e2
Mikhail Kucherenko <mihail.k.work@mail.ru>
2023-05-28 18:14:11 +0300

    advanced-destructuring-1

---
+++ src/js/__tests__/testApp.js
@@ -0,0 +1,71 @@
+import getArray from '../app';
+
+test('Возвращаем массив с двумя объектами, у одного из - Описание недоступно', () => {
+  const hero = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+        // <- обратите внимание, описание "засекречено"
+      },
+    ],
+  };
+  const unit = getArray(hero);
+  const result = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+  expect(unit).toEqual(result);
+});
+
+test('special пустой, должен вернуть пустой массив', () => {
+  const hero = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [],
+  };
+  const unit = getArray(hero);
+  const result = [];
+  expect(unit).toEqual(result);
+});
+
+test('special отсутствует, должен вернуть пустой массив', () => {
+  const hero = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+  };
+  const unit = getArray(hero);
+  const result = [];
+  expect(unit).toEqual(result);
+});
---
+++ src/js/app.js
@@ -0,0 +1,67 @@
+export default function getArray(characterObject) {
+  const result = [];
+  const { special } = characterObject;
+  if (!special) {
+    return [];
+  }
+
+  for (let i = 0; i < special.length; i += 1) {
+    if (!special[i].description) {
+      special[i].description = 'Описание недоступно';
+    }
+    result.push({
+      id: special[i].id,
+      name: special[i].name,
+      icon: special[i].icon,
+      description: special[i].description,
+    });
+  }
+
+  return result;
+}
+
+const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
+};
+
+const character1 = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [],
+};
+
+const character2 = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+};
+
+console.log(getArray(character));
+console.log(getArray(character1));
+console.log(getArray(character2));
